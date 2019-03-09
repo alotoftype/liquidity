13ad6f9530b0e03816b1064cd7f6d6c050d74171
smjr <alotoftype@gmail.com>
2019-03-09 11:19:20 -0500

    removing unnecessary files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import  css from "../sass/styles.scss"
 import Header from "../components/header"
+import Footer from "../components/footer"
 
 export default()  => <div className={css.example}>
 <Header />
-<p>Welcome to Next.js!</p></div>
+<p>Welcome to Next.js!</p>
+<Footer />
+</div>
  
