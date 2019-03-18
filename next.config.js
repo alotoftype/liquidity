548f252a669ab41f7f3260a95a5fe848ce8cd558
smjr <alotoftype@gmail.com>
2019-03-18 07:40:32 -0400

    updates to configuration files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css')
 const withPlugins = require('next-compose-plugins');
 const withFonts  = require('next-fonts');
 const withImages = require('next-images');
